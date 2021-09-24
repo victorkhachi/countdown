d7dad94aee3ae658b4bd56758107f9be47444502
victorkhachi <victorkhachi@gmail.com>
2021-09-24 10:28:08 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 // import {BrowserRouter as Router , Switch, Route} from 'react-router-dom'
 import './App.css';
-// import Countdown from './countdown/countdown';
-import Confirm from './Email/confirm/confirm';
+// import Multiple from './Email/confirm/multiple';
+import Countdown from './countdown/countdown';
+// import Confirm from './Email/confirm/confirm';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
-        <Switch>
-           <Route to='/counter'>
+     
             <Countdown />
-            </Route>
-          <Route to='/confirm'> */}
-            <Confirm />
-          {/* </Route>
-
-          </Switch>
-        </Router> */}
+       
     </div>
   );
 }
--- src/Email/confirm/confirm.jsx
+++ src/Email/confirm/confirm.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import logo from './images/logo.png'
-import './confirm.scss'
+// import React, { useState,useEffect } from 'react'
+// import logo from './images/logo.png'
+// import './confirm.scss'
 
-export default function Confirm() {
-    return (
-        <div class='email-template'>
-           <head><img src={logo} alt=''/></head>
-           <body>
-               <h2>Confirm your registration</h2>
-               <p class='name'>hello dimgba</p>
-               <p>thank you for joing our community</p>
-                  
-                <div class='blue'>your private key is:</div>
-                <div class='key'>nfigiowetigweirgmieormgvnekntio</div>
-                <div class='blue'>your wallet address is:</div>
-                <div class='key'>fnbvjngtuenwutnvntugnewrg ewugegnet</div>
-                <p>please save these details</p>
-                <p>kindly confirm your email address by clicking the link below</p>
+// export default function Confirm() {
+//     const [content,setContent]=useState()
+//     const confirm = {
+//         title:'Confirm your registration',
+//         body:<div>
+//         <p>thank you for joing our community</p>
 
-                <button>
-                Verify Email Address
-                </button>
-                <div class='greeting'><span>Best,</span><p>SWIRGE team</p></div>
-               </body> 
+//         <div class='blue'>your private key is:</div>
+//         <div class='key'>nfigiowetigweirgmieormgvnekntio</div>
+//         <div class='blue'>your wallet address is:</div>
+//         <div class='key'>fnbvjngtuenwutnvntugnewrg ewugegnet</div>
+//         <p>please save these details</p>
+//         <p>kindly confirm your email address by clicking the link below</p>
 
-               <footer>Copyright 2021 SWIRGE.All rights reserved.</footer>
-        </div>
-    )
-}
+//         <button>
+//             Verify Email Address
+//         </button>
+//     </div>}
+//     const multiple = {
+//         title:'multiple login attempts',
+//         body:<div>
+//         <p>We noticed there has been multiple attempts at accessing your account,</p>
+//         <p>Kindly confirm if you are the one trying to sign in</p>
+//         <button>Yes, it was me</button>
+//         <p>If it wasn’t you, change your password and sign out of all instances with the button below</p>
+//         <button class='not'>Change password</button>
+//         </div>
+//     }
+
+//     useEffect(()=>setContent(multiple))
+//     return (
+//         <div class='email-template'>
+//            <head><img src={logo} alt=''/></head>
+//            <body>
+//                <h2>{content.title}</h2>
+//                <p class='name'>hello dimgba</p>
+//                {content.body}
+//                 <div class='greeting'><span>Best,</span><p>SWIRGE team</p></div>
+//                </body> 
+
+//                <footer>Copyright 2021 SWIRGE.All rights reserved.</footer>
+//         </div>
+//     )
+// }
